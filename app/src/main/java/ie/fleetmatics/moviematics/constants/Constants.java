dafa4ec21940432d2bfb7ef96df6bda66b545df4
Diego Gómez Alonso <diego.gomez.alonso@gmail.com>
2020-05-14 10:05:20 +0200

    Update Constants.java

--- app/src/main/java/ie/fleetmatics/moviematics/constants/Constants.java
+++ app/src/main/java/ie/fleetmatics/moviematics/constants/Constants.java
@@ -10,6 +10,6 @@ public class Constants {
     public static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/";
 
     // API KEY
-    public static final String API_KEY = "76e820d1f6a93db9e0e12c04a69fc6d1";
+    public static final String API_KEY = "your api key";
 
 }
